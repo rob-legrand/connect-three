ad53d5beb72b7a3a8aeb5fe5bbc75cd50e4a3674
Rob LeGrand <roblegrand98@gmail.com>
2024-12-05 15:16:22 -0600

    Consolidate var declarations

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-/*jslint browser: true, vars: true, indent: 3 */
+/*jslint browser: true, indent: 3 */
 
-(function () {
+document.addEventListener('DOMContentLoaded', function () {
    'use strict';
+   var aiLevel, clickFunc, disableDropButtons, dropButtons, dropPieceInColumn, fixDropButtons, fixShowHints, getBoard, hints, makeMoveOnBoard, maxNumColumns, maxNumRows, minimaxBoardValue, minimaxMove, newGame, numColumns, numHumanPlayers, numRows, numSpacesToWin, piece0, piece1, piece2, playerToMoveNext, resizeBoard, selectAiLevelElement, selectNumColumnsElement, selectNumRowsElement, showHintsCheckbox, spaces, unmakeMoveOnBoard, winnerOnBoard;
 
-   var maxNumColumns = 5;
-   var maxNumRows = 5;
-   var selectNumColumnsElement = document.getElementById('select-num-columns');
-   var numColumns = parseInt(selectNumColumnsElement.options[selectNumColumnsElement.selectedIndex].value, 10);
-   var selectNumRowsElement = document.getElementById('select-num-rows');
-   var numRows = parseInt(selectNumRowsElement.options[selectNumRowsElement.selectedIndex].value, 10);
-   var selectAiLevelElement = document.getElementById('select-ai-level');
-   var aiLevel = parseInt(selectAiLevelElement.options[selectAiLevelElement.selectedIndex].value, 10);
-   var showHintsCheckbox = document.getElementById('show-hints');
-   var numSpacesToWin = 3;
-   var numHumanPlayers = 1;
-   var playerToMoveNext;
-   var dropButtons = [document.getElementById('drop1'), document.getElementById('drop2'), document.getElementById('drop3'), document.getElementById('drop4'), document.getElementById('drop5')];
-   var spaces = [[document.getElementById('r1c1'), document.getElementById('r2c1'), document.getElementById('r3c1'), document.getElementById('r4c1'), document.getElementById('r5c1')],
-                 [document.getElementById('r1c2'), document.getElementById('r2c2'), document.getElementById('r3c2'), document.getElementById('r4c2'), document.getElementById('r5c2')],
-                 [document.getElementById('r1c3'), document.getElementById('r2c3'), document.getElementById('r3c3'), document.getElementById('r4c3'), document.getElementById('r5c3')],
-                 [document.getElementById('r1c4'), document.getElementById('r2c4'), document.getElementById('r3c4'), document.getElementById('r4c4'), document.getElementById('r5c4')],
-                 [document.getElementById('r1c5'), document.getElementById('r2c5'), document.getElementById('r3c5'), document.getElementById('r4c5'), document.getElementById('r5c5')]];
-   var hints = [document.getElementById('hint1'), document.getElementById('hint2'), document.getElementById('hint3'), document.getElementById('hint4'), document.getElementById('hint5')];
+   maxNumColumns = 5;
+   maxNumRows = 5;
+   selectNumColumnsElement = document.getElementById('select-num-columns');
+   numColumns = parseInt(selectNumColumnsElement.options[selectNumColumnsElement.selectedIndex].value, 10);
+   selectNumRowsElement = document.getElementById('select-num-rows');
+   numRows = parseInt(selectNumRowsElement.options[selectNumRowsElement.selectedIndex].value, 10);
+   selectAiLevelElement = document.getElementById('select-ai-level');
+   aiLevel = parseInt(selectAiLevelElement.options[selectAiLevelElement.selectedIndex].value, 10);
+   showHintsCheckbox = document.getElementById('show-hints');
+   numSpacesToWin = 3;
+   numHumanPlayers = 1;
+   dropButtons = [document.getElementById('drop1'), document.getElementById('drop2'), document.getElementById('drop3'), document.getElementById('drop4'), document.getElementById('drop5')];
+   spaces = [[document.getElementById('r1c1'), document.getElementById('r2c1'), document.getElementById('r3c1'), document.getElementById('r4c1'), document.getElementById('r5c1')],
+             [document.getElementById('r1c2'), document.getElementById('r2c2'), document.getElementById('r3c2'), document.getElementById('r4c2'), document.getElementById('r5c2')],
+             [document.getElementById('r1c3'), document.getElementById('r2c3'), document.getElementById('r3c3'), document.getElementById('r4c3'), document.getElementById('r5c3')],
+             [document.getElementById('r1c4'), document.getElementById('r2c4'), document.getElementById('r3c4'), document.getElementById('r4c4'), document.getElementById('r5c4')],
+             [document.getElementById('r1c5'), document.getElementById('r2c5'), document.getElementById('r3c5'), document.getElementById('r4c5'), document.getElementById('r5c5')]];
+   hints = [document.getElementById('hint1'), document.getElementById('hint2'), document.getElementById('hint3'), document.getElementById('hint4'), document.getElementById('hint5')];
    spaces[0][0].src = 'stags.gif';
-   var piece1 = spaces[0][0].src;
+   piece1 = spaces[0][0].src;
    spaces[0][0].src = 'hawks.gif';
-   var piece2 = spaces[0][0].src;
+   piece2 = spaces[0][0].src;
    spaces[0][0].src = 'empty.gif';
-   var piece0 = spaces[0][0].src;
+   piece0 = spaces[0][0].src;
 
-   var resizeBoard = function () {
+   resizeBoard = function () {
       var column, row;
       for (column = 0; column < maxNumColumns; column += 1) {
          dropButtons[column].style.display = column < numColumns ? '' : 'none';
@@ -41,21 +41,21 @@
    };
    resizeBoard();
 
-   var disableDropButtons = function () {
+   disableDropButtons = function () {
       var column;
       for (column = 0; column < numColumns; column += 1) {
          dropButtons[column].disabled = true;
       }
    };
 
-   var fixDropButtons = function () {
+   fixDropButtons = function () {
       var column;
       for (column = 0; column < numColumns; column += 1) {
          dropButtons[column].disabled = spaces[column][numRows - 1].src !== piece0;
       }
    };
 
-   var fixShowHints = function () {
+   fixShowHints = function () {
       var column;
       for (column = 0; column < numColumns; column += 1) {
          hints[column].style.display = showHintsCheckbox.checked ? '' : 'none';
@@ -63,7 +63,7 @@
    };
    fixShowHints();
 
-   var dropPieceInColumn = function (column) {
+   dropPieceInColumn = function (column) {
       var row;
       if (column >= 0 && column < numColumns) {
          for (row = 0; row < numRows; row += 1) {
@@ -76,9 +76,9 @@
       return false;
    };
 
-   var getBoard = function () {
-      var column, row;
-      var board = [];
+   getBoard = function () {
+      var board, column, row;
+      board = [];
       for (column = 0; column < numColumns; column += 1) {
          board[column] = [];
          for (row = 0; row < numRows; row += 1) {
@@ -94,7 +94,7 @@
       return board;
    };
 
-   var winnerOnBoard = function (board) {
+   winnerOnBoard = function (board) {
       var column, row, winner, whichSpace;
       if (numRows >= numSpacesToWin) {
          for (column = 0; column < numColumns; column += 1) {
@@ -164,7 +164,7 @@
       return 3;
    };
 
-   var makeMoveOnBoard = function (board, player, column) {
+   makeMoveOnBoard = function (board, player, column) {
       var row;
       if (column >= 0 && column < numColumns) {
          for (row = 0; row < numRows; row += 1) {
@@ -177,7 +177,7 @@
       return false;
    };
 
-   var unmakeMoveOnBoard = function (board, column) {
+   unmakeMoveOnBoard = function (board, column) {
       var row;
       if (column >= 0 && column < numColumns) {
          for (row = numRows - 1; row >= 0; row -= 1) {
@@ -190,41 +190,42 @@
       return false;
    };
 
-   var minimaxBoardValue = function minimaxBoardValue(board, playerToMoveNext, depth) {
-      var bestOutcome = playerToMoveNext === 1 ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
-      var column, outcome;
+   minimaxBoardValue = function minimaxBoardValue(board, playerToMoveNext, depth) {
+      var bestOutcome, column, outcome, value;
+      bestOutcome = playerToMoveNext === 1 ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
       if (winnerOnBoard(board) === 1) {
-         return 1;
+         return {minimaxOutcome: 1, minimaxDepth: depth};
       }
       if (winnerOnBoard(board) === 2) {
-         return -1;
+         return {minimaxOutcome: -1, minimaxDepth: depth};
       }
       if (winnerOnBoard(board) === 3) {
-         return 0;
+         return {minimaxOutcome: 0, minimaxDepth: depth};
       }
       if (depth === 0) {
-         return 0;
+         return {minimaxOutcome: 0, minimaxDepth: depth};
       }
       for (column = 0; column < numColumns; column += 1) {
          if (makeMoveOnBoard(board, playerToMoveNext, column)) {
-            outcome = minimaxBoardValue(board, playerToMoveNext === 1 ? 2 : 1, depth - 1);
+            value = minimaxBoardValue(board, playerToMoveNext === 1 ? 2 : 1, depth - 1);
+            outcome = value.minimaxOutcome;
             if (playerToMoveNext === 1 ? outcome > bestOutcome : outcome < bestOutcome) {
                bestOutcome = outcome;
             }
             unmakeMoveOnBoard(board, column);
          }
       }
-      return bestOutcome;
+      return {minimaxOutcome: bestOutcome, minimaxDepth: 98};
    };
 
-   var minimaxMove = function () {
-      var bestColumns = [];
-      var bestOutcome = playerToMoveNext === 1 ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
-      var board = getBoard();
-      var column, outcome;
+   minimaxMove = function () {
+      var bestColumns, bestOutcome, board, column, outcome;
+      bestColumns = [];
+      bestOutcome = playerToMoveNext === 1 ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY;
+      board = getBoard();
       for (column = 0; column < numColumns; column += 1) {
          if (makeMoveOnBoard(board, playerToMoveNext, column)) {
-            outcome = aiLevel > 0 ? minimaxBoardValue(board, playerToMoveNext === 1 ? 2 : 1, aiLevel - 1) : 0;
+            outcome = aiLevel > 0 ? minimaxBoardValue(board, playerToMoveNext === 1 ? 2 : 1, aiLevel - 1).minimaxOutcome : 0;
             if (playerToMoveNext === 1 ? outcome > bestOutcome : outcome < bestOutcome) {
                bestColumns = [column];
                bestOutcome = outcome;
@@ -237,7 +238,7 @@
       return bestColumns[Math.floor(bestColumns.length * Math.random())];
    };
 
-   var newGame = function newGame() {
+   newGame = function newGame() {
       var column, row, winner;
       for (column = 0; column < numColumns; column += 1) {
          for (row = 0; row < numRows; row += 1) {
@@ -249,10 +250,9 @@
    };
    newGame();
 
-   var clickFunc = function (column) {
+   clickFunc = function (column) {
       return function () {
-         var winner;
-         var board, val, c;
+         var board, c, val, winner;
          dropPieceInColumn(column);
          for (c = 0; c < numColumns; c += 1) {
             hints[c].innerHTML = '';
@@ -295,7 +295,7 @@
          for (c = 0; c < numColumns; c += 1) {
             board = getBoard();
             if (makeMoveOnBoard(board, playerToMoveNext, c)) {
-               val = minimaxBoardValue(board, playerToMoveNext === 1 ? 2 : 1, numColumns <= 3 ? 15 : numColumns <= 4 ? 9 : 6);
+               val = minimaxBoardValue(board, playerToMoveNext === 1 ? 2 : 1, numColumns <= 3 ? 15 : numColumns <= 4 ? 9 : 6).minimaxOutcome;
                if (val < -0.5) {
                   hints[c].innerHTML = 'L';
                } else if (val > 0.5) {
@@ -311,27 +311,29 @@
       };
    };
 
-   var column;
-   for (column = 0; column < maxNumColumns; column += 1) {
-      dropButtons[column].onclick = clickFunc(column);
-   }
+   (function () {
+      var column;
+      for (column = 0; column < maxNumColumns; column += 1) {
+         dropButtons[column].addEventListener('click', clickFunc(column));
+      }
+   }());
 
-   selectNumColumnsElement.onchange = function () {
+   selectNumColumnsElement.addEventListener('change', function () {
       numColumns = parseInt(selectNumColumnsElement.options[selectNumColumnsElement.selectedIndex].value, 10);
       resizeBoard();
       newGame();
-   };
+   });
 
-   selectNumRowsElement.onchange = function () {
+   selectNumRowsElement.addEventListener('change', function () {
       numRows = parseInt(selectNumRowsElement.options[selectNumRowsElement.selectedIndex].value, 10);
       resizeBoard();
       newGame();
-   };
+   });
 
-   selectAiLevelElement.onchange = function () {
+   selectAiLevelElement.addEventListener('change', function () {
       aiLevel = parseInt(selectAiLevelElement.options[selectAiLevelElement.selectedIndex].value, 10);
-   };
+   });
 
-   showHintsCheckbox.onclick = fixShowHints;
-   document.getElementById('new-game').onclick = newGame;
-}());
+   showHintsCheckbox.addEventListener('click', fixShowHints);
+   document.getElementById('new-game').addEventListener('click', newGame);
+});
